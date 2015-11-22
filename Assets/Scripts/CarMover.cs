e4216b028f8794c395aec8e9e99ad3f4170b50bc
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2015-11-22 00:02:36 -0500

    Ported car and path movers from HW3

---
+++ Assets/Scripts/CarMover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Stops if the player gets in the way.
+public class CarMover : PathMover {
+
+	// Whether the car has stopped due to the player being in the way.
+	[HideInInspector]
+	public bool stopped = false;
+	// The moving speed of the car.
+	float startSpeed;
+	// The amount that speed is changed every tick when changing states.
+	float speedIncrement;
+
+	// Use this for initialization.
+	new void Start () {
+		startSpeed = speed;
+		speedIncrement = startSpeed / 40;
+		base.Start ();
+	}
+	
+	// Update is called once per frame.
+	new void Update () {
+		if (stopped) {
+			if (speed > 0) {
+				speed -= speedIncrement;
+			} else if (speed < 0) {
+				speed = 0;
+			}
+		} else if (speed < startSpeed) {
+			speed += speedIncrement;
+		}
+		base.Update ();
+	}
+}
---
+++ Assets/Scripts/CarVision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handles stopping the car when the player is in the way.
+public class CarVision : MonoBehaviour {
+
+	// The car component of the car.
+	CarMover car;
+
+	// Use this for initialization.
+	void Start () {
+		car = GetComponent<CarMover> ();
+	}
+	
+	// Stops the car and plays a sound when the player is spotted.
+	void OnTriggerEnter (Collider collider) {
+		if (collider.tag == "Player" && !car.stopped) {
+			car.stopped = true;
+		}
+	}
+	
+	// Resumes car motion when the player gets out of the way.
+	void OnTriggerExit (Collider collider) {
+		if (collider.tag == "Player") {
+			car.stopped = false;
+		}
+	}
+}
